2046873c1c360d3f175e4821f69dee5c06289962
capgja16ja05 <82081812+capgja16ja05@users.noreply.github.com>
2021-04-14 09:44:49 +0530

    Add files via upload

---
+++ task1/Camera.java
@@ -0,0 +1,31 @@
+package day6.task1;
+
+public class Camera extends ElectronicDevice implements Bluetooth{
+
+	
+	
+	@Override
+	public void doBluetoothConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doSHareFiles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doSwitchOn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doSwitchOff() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
